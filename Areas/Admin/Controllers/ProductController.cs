167dc64aca03cbbb5d15b149b3eb7306fae67b63
Liangyew <wongly280@gmail.com>
2023-03-28 23:10:26 +0100

    Add project files.

---
+++ Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,215 @@
+﻿using BulkyBook.Models;
+using Microsoft.AspNetCore.Mvc;
+using BulkyBookWeb.DataAccess;
+using BulkyBook.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using BulkyBook.Models.ViewModels;
+
+namespace BulkyBookWeb.Areas.Admin.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _hostEnvironment;
+
+
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment hostEnvironment)
+        {
+            _unitOfWork = unitOfWork;
+            _hostEnvironment = hostEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+        //GET
+        public IActionResult Upsert(int? id)
+        {
+            ProductViewModel productViewModel = new()
+            {
+                Product = new(),
+                CategoryList = _unitOfWork.Category.GetAll().Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString(),
+                }),
+                CoverTypeList = _unitOfWork.CoverType.GetAll().Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString(),
+                }),
+            };
+            ViewBag.Id = id;
+          
+           
+        
+            if (id == null || id == 0)
+            {
+                //Create product
+                return View(productViewModel);
+            }
+            else
+            {
+                //update product
+                productViewModel.Product = _unitOfWork.Product.GetFirstOrDefault(u=>u.Id == id);
+                return View(productViewModel);
+            }
+
+           
+        }
+   
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(ProductViewModel obj, IFormFile file)
+        {
+            if (ModelState.IsValid)
+            {
+                string wwwRootPath = _hostEnvironment.WebRootPath;
+                if (file != null)
+                {
+                    string fileName = Guid.NewGuid().ToString();
+                    var uploads = Path.Combine(wwwRootPath, @"images\Products");
+                    var extension = Path.GetExtension(file.FileName);
+
+                    if(obj.Product.ImageUrl != null)
+                    {
+                        var oldImagePath = Path.Combine(wwwRootPath, obj.Product.ImageUrl.TrimStart('\\'));
+                        if (System.IO.File.Exists(oldImagePath))
+                        {
+                            System.IO.File.Delete(oldImagePath);
+                        }
+                    }
+
+                    using(var fileStreams = new FileStream(Path.Combine(uploads, fileName + extension), FileMode.Create))
+                    {
+                        file.CopyTo(fileStreams);
+                    }
+                    obj.Product.ImageUrl = @"\images\Products\" + fileName + extension;
+                }
+                if (obj.Product.Id == 0)
+                {
+                    _unitOfWork.Product.Add(obj.Product);
+
+                }
+                else
+                {
+                    _unitOfWork.Product.Update(obj.Product);
+                }
+                //_unitOfWork.ProductViewModel.Add(obj);
+           
+                _unitOfWork.Save();
+                TempData["success"] = obj.Product.Id == 0 ? "Category created successfully" : "Category updated successfully";
+                return RedirectToAction("Index");
+            }
+ 
+            obj.CategoryList = _unitOfWork.Category.GetAll().Select(i => new SelectListItem
+            {
+                Text = i.Name,
+                Value = i.Id.ToString(),
+            });
+            obj.CoverTypeList = _unitOfWork.CoverType.GetAll().Select(i => new SelectListItem
+            {
+                Text = i.Name,
+                Value = i.Id.ToString(),
+            });
+
+            return View(obj);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var productList = _unitOfWork.Product.GetAll(includeProperties:"Category,CoverType");
+            return Json(new { data = productList });
+
+        }
+        //POST
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+            var obj = _unitOfWork.Product.GetFirstOrDefault(u => u.Id == id);
+            if (obj == null)
+            {
+
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+            var oldImagePath = Path.Combine(_hostEnvironment.WebRootPath, obj.ImageUrl.TrimStart('\\'));
+            if (System.IO.File.Exists(oldImagePath))
+            {
+                System.IO.File.Delete(oldImagePath);
+            }
+
+            _unitOfWork.Product.Remove(obj);
+            _unitOfWork.Save();
+            return Json(new { success = true, message = "Delete successful" });
+
+        }
+
+        ////GET
+        //public IActionResult Edit(int? id)
+        //{
+        //    if (id == null || id == 0)
+        //    {
+        //        return NotFound();
+        //    }
+        //    var categoryFromDb = _unitOfWork.Category.GetFirstOrDefault(u => u.Id == id);
+        //    //var categoryFromDbFirst = _db.Categories.FirstOrDefault(c => c.Id == id);
+        //    //var categoryFromDbSingle = _db.Categories.SingleOrDefault(c => c.Id == id);
+
+        //    if (categoryFromDb == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return View(categoryFromDb);
+        //}
+        ////POST
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public IActionResult Edit(Category obj)
+        //{
+        //    if (obj.Name == obj.DisplayOrder.ToString())
+        //    {
+        //        ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name");
+        //    }
+        //    if (ModelState.IsValid)
+        //    {
+        //        _unitOfWork.Category.Update(obj);
+        //        _unitOfWork.Save();
+        //        TempData["success"] = "Category updated successfully";
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View(obj);
+        //}
+
+        ////GET
+        //public IActionResult Delete(int? id)
+        //{
+        //    if (id == null || id == 0)
+        //    {
+        //        return NotFound();
+        //    }
+        //    var categoryFromDb = _unitOfWork.Category.GetFirstOrDefault(u => u.Id == id);
+
+        //    if (categoryFromDb == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return View(categoryFromDb);
+        //}
+        ////POST
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public IActionResult DeletePOST(int? id)
+        //{
+        //    var obj = _unitOfWork.Category.GetFirstOrDefault(u => u.Name == "id");
+        //    _unitOfWork.Category.Remove(obj);
+        //    _unitOfWork.Save();
+        //    TempData["success"] = "Category deleted successfully";
+        //    return RedirectToAction("Index");
+
+        //}
+    }
+}
